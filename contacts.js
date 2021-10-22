cf00d91f604485b3d1f2e5920087cccfd9d5f49d
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-10-22 20:07:10 +0300

    Небольшие правки в команде add

--- contacts.js
+++ contacts.js
@@ -1,6 +1,3 @@
-const fs = require('fs/promises')
-const path = require('path')
-
 // const { v4 } = require('uuid')
 // console.log(v4())
 // console.log(crypto)
@@ -13,13 +10,8 @@ function removeContact(contactId) {
   // ...твой код
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
-
 module.exports = {
   //   listContacts,
   getContactById,
   removeContact,
-  addContact,
 }
--- db/addContact.js
+++ db/addContact.js
@@ -8,10 +8,9 @@ const listContacts = require('./listContacts')
 const addContact = async (name, email, phone) => {
   try {
     const contacts = await listContacts()
-    const newContact = { id: v4(), name, email, phone }
+    const newContact = { id: contacts.length + 1, name, email, phone }
     contacts.push(newContact)
-    await fs.readFile(contactsPath, 'utf-8')
-    JSON.stringify(contacts, null, 2)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
     return newContact
   } catch (error) {
     console.error(chalk.red(error.message))
